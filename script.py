8962335dc5dc417d7c976940f1293bc08af2daec
braamconradie <braam.conradie@gmail.com>
2023-04-24 23:36:36 +0000

    Gsheet stuff working and requirements file there

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print('hello world')
+#need to pip install 
+# pip3 install -r requirements.txt 
+
+
+
+# get the spreadsheet
+import gspread
+
+# check stuff is working
+# gc = gspread.service_account(filename='credentials.json')
+# sh = gc.open_by_key('1B0plp_7tp5cXrN1ce1JcFG5e9RnLjBSJ-cIeUGS_rcE')
+# worksheet = sh.sheet1 
+# res = worksheet.get_all_records()
+# print(res)
